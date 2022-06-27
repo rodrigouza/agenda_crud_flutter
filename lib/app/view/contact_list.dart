c5a41de37e4eece8a229b1ffe146d37b1c02518c
Rodrigo Elias de Souza <92738606+rodrigouza@users.noreply.github.com>
2022-06-27 12:57:57 -0300

    Create contact_list.dart

---
+++ lib/app/view/contact_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ContactList extends StatelessWidget {
+  const ContactList({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
